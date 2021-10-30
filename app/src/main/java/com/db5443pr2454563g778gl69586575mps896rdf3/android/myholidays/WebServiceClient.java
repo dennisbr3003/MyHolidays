3378ae4835c6ce87dcbaab54c19393ac69bd4382
Dennis Brink <dennis.mjknd@gmail.com>
2021-10-30 22:33:18 +0200

    Replaced AsyncTask (deprecated) in WebServiceClient

--- app/src/main/java/com/db5443pr2454563g778gl69586575mps896rdf3/android/myholidays/WebServiceClient.java
+++ app/src/main/java/com/db5443pr2454563g778gl69586575mps896rdf3/android/myholidays/WebServiceClient.java
@@ -54,102 +54,91 @@ public class WebServiceClient implements IWebService,ISharedRef{
         final AlertDialog dlg = Message.ProgressDialog.getProgressDialog(context, new Result(context.getString(R.string.DownloadingUserData)));
         dlg.show();
 
-        new AsyncTask<Void, Void, Void>(){
+        AsyncTask.execute(() -> {
+            OkHttpClient client = new OkHttpClient().newBuilder().build();
 
-            protected Void doInBackground(Void... voids) {
+            // okHttp3 does not support a body for GET, using the device_id as a path variable -->
+            Request request = new Request.Builder()
+                    .url(WebServiceClient.this.getUrlObject())
+                    .method("GET", null)
+                    .build();
 
-                OkHttpClient client = new OkHttpClient().newBuilder().build();
+            Log.d("DENNIS_B", "okhttp3 url (by object): " + request.url().toString());
 
-                // okHttp3 does not support a body for GET, using the device_id as a path variable -->
-                Request request = new Request.Builder()
-                        .url(getUrlObject())
-                        .method("GET", null)
-                        .build();
+            try {
+                Thread.sleep(1000); // fake slow download so the dialog will show
+            } catch (InterruptedException e) {
+                WebServiceClient.this.showSyncErrorDialog(e.getMessage());
+            }
 
-                Log.d("DENNIS_B", "okhttp3 url (by object): " + request.url().toString());
+            client.newCall(request).enqueue(new Callback() {
 
-                try {
-                    Thread.sleep(1000); // fake slow download so the dialog will show
-                } catch (InterruptedException e) {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull final IOException e) {
+                    dlg.dismiss();
                     showSyncErrorDialog(e.getMessage());
                 }
 
-                client.newCall(request).enqueue(new Callback() {
+                // call m.b.v.enqueue is zelf asynchrone, onPostExecute is hier dan niet nodig; alles gaat via de callback
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) {
 
-                    @Override
-                    public void onFailure(@NotNull Call call, @NotNull final IOException e) {
+                    try {
+                        // first cast the answer to a json object for easy handling -->
+                        String JsonResponse = response.body().string();
+                        if (JsonResponse.startsWith("[")) {
+                            //this cannot be supported for now
+                            Log.d("DENNIS_B", "Response is an JSON array (not supported for now)");
+                            dlg.dismiss();
+                            showSyncErrorDialog("Unsupported response format. Expected JsonObject, received JsonArray");
+                            return;
+                        }
+                        JSONObject j_object = new JSONObject(JsonResponse);
+
+                        Log.d("DENNIS_B", "Has signature key " + j_object.has(SIGNATURE_KEY));
+
+                        if (j_object.has(SIGNATURE_KEY)) {
+
+                            // Now cast the answer to the expected format by using a pojo -->
+                            ObjectMapper mapper = new ObjectMapper();
+                            ContentRoot contentRoot = mapper.readValue(j_object.toString(), ContentRoot.class);
+                            Log.d("DENNIS_B", "contentRoot not null? " + (contentRoot != null));
+
+                            if (contentRoot != null) {
+                                Log.d("DENNIS_B", "webEventListener not null? " + (webEventListener != null));
+                                if (webEventListener != null) {
+                                    Log.d("DENNIS_B", "sending answer (contentRoot) to webEventListener");
+                                    webEventListener.loadDownLoadedUserData(contentRoot, dlg);
+                                }
+                            } else {
+                                dlg.dismiss();
+                                showSyncErrorDialog("data did not download successfully. Nothing was retrieved");
+                            }
+
+                        } else { // error object is returned
+                            dlg.dismiss();
+                            if (j_object.has(SIGNATURE_FIELD)) {
+                                showSyncErrorDialog(j_object.getString(SIGNATURE_FIELD));
+                            } else {
+                                showSyncErrorDialog("No signature key found in answer");
+                            }
+                        }
+                    } catch (JSONException e) {
+                        dlg.dismiss();
+                        Log.d("DENNIS_B", "error casting answer to JSON: " + e.getMessage());
+                        showSyncErrorDialog(e.getMessage());
+                    } catch (Exception e) {
+                        Log.d("DENNIS_B", "error casting answer to pojo " + e.getMessage());
                         dlg.dismiss();
                         showSyncErrorDialog(e.getMessage());
                     }
-
-                    // call m.b.v.enqueue is zelf asynchrone, onPostExecute is hier dan niet nodig; alles gaat via de callback
-                    @Override
-                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-
-                        try {
-                            // first cast the answer to a json object for easy handling -->
-                            String JsonResponse = response.body().string();
-                            if(JsonResponse.startsWith("[")){
-                                //this cannot be supported for now
-                                Log.d("DENNIS_B","Response is an JSON array (not supported for now)");
-                                dlg.dismiss();
-                                showSyncErrorDialog("Unsupported response format. Expected JsonObject, received JsonArray");
-                                return;
-                            }
-                            JSONObject j_object = new JSONObject(JsonResponse);
-
-                            Log.d("DENNIS_B","Has signature key " + j_object.has(SIGNATURE_KEY));
-
-                            if (j_object.has(SIGNATURE_KEY)) {
-
-                                // Now cast the answer to the expected format by using a pojo -->
-                                ObjectMapper mapper = new ObjectMapper();
-                                ContentRoot contentRoot = mapper.readValue(j_object.toString(), ContentRoot.class);
-                                Log.d("DENNIS_B","contentRoot not null? " + (contentRoot != null));
-
-                                if (contentRoot != null){
-                                    Log.d("DENNIS_B","webEventListener not null? " + (webEventListener != null));
-                                    if (webEventListener != null) {
-                                        Log.d("DENNIS_B", "sending answer (contentRoot) to webEventListener");
-                                        webEventListener.loadDownLoadedUserData(contentRoot, dlg);
-                                    }
-                                } else {
-                                    dlg.dismiss();
-                                    showSyncErrorDialog("data did not download successfully. Nothing was retrieved");
-                                }
-
-                            } else { // error object is returned
-                                dlg.dismiss();
-                                if (j_object.has(SIGNATURE_FIELD)) {
-                                    showSyncErrorDialog(j_object.getString(SIGNATURE_FIELD));
-                                }else {
-                                    showSyncErrorDialog("No signature key found in answer");
-                                }
-                            }
-                        } catch (JSONException e) {
-                            dlg.dismiss();
-                            Log.d("DENNIS_B","error casting answer to JSON: " + e.getMessage());
-                            showSyncErrorDialog(e.getMessage());
-                        }catch(Exception e){
-                            Log.d("DENNIS_B","error casting answer to pojo " + e.getMessage());
-                            dlg.dismiss();
-                            showSyncErrorDialog(e.getMessage());
-                        }
-                    }
-                });
-                return null;
-            }
-        }.execute();
-
+                }
+            });
+        });
     }
 
     private void showSyncErrorDialog(final String message){
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                Message.ErrorDialog.getErrorMessageDialog(context, new Result(message)).show();
-            }
-        });
+        mHandler.post(() -> Message.ErrorDialog.getErrorMessageDialog(context, new Result(message)).show());
     }
 
     private HttpUrl getUrlObject(){
